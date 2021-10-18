2c9d8c5ee9ed9bf6a1a3626c5ffec2402cb498d3
Omer <omerfaruk.bjk@hotmail.com>
2021-10-18 11:38:33 +0300

    Add files via upload

---
+++ disable_turbo.bat
@@ -0,0 +1,4 @@
+
+powercfg -setacvalueindex SCHEME_MIN SUB_PROCESSOR PROCTHROTTLEMAX 99
+powercfg -setacvalueindex SCHEME_MIN SUB_PROCESSOR PROCTHROTTLEMIN 99
+powercfg.exe -setactive SCHEME_MIN
---
+++ enable_turbo.bat
@@ -0,0 +1,4 @@
+
+powercfg -setacvalueindex SCHEME_MIN SUB_PROCESSOR PROCTHROTTLEMAX 100
+powercfg -setacvalueindex SCHEME_MIN SUB_PROCESSOR PROCTHROTTLEMIN 100
+powercfg.exe -setactive SCHEME_MIN
